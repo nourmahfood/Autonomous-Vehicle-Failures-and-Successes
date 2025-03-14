2f05b9aa8f673c58cb607becb91aebdac2da1ae3
nourmahfood <nourmahfood7@gmail.com>
2025-03-14 23:08:19 +0200

    Update db.php for local database connection

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 try {
     // Corrected PDO connection with the proper port
-    $conn = new PDO("mysql:host=127.0.0.1;port=3307;dbname=mystore", "root", "");
+    $conn = new PDO("mysql:host=127.0.0.1;port=3306;dbname=mystore", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     // echo "Connected successfully!<br>";
 } catch (PDOException $e) {
